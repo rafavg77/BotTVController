71f64ccaf94c2e966744ca5fe0611678fc590564
rafavg77 <rafavg77@gmail.com>
2020-05-17 05:38:10 -0500

    Se agrega funcionalidad Schedule

--- src/bot.py
+++ src/bot.py
@@ -4,12 +4,18 @@
 import re
 import os
 import logging
+import requests
+import schedule
+import time
+import subprocess
+from threading import Timer 
 from catt.api import CattDevice
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 BOT_TEL_KEY=os.environ['BOT_TEL_KEY']
 BOT_NGR_KEY=os.environ['BOT_NGR_KEY']
 CAST_DEVICE="TV Recamara"
+CHAT_ID="32268671"
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -35,9 +41,35 @@ def cast(update, context):
 		 cast.play_url(video, resolve=True, block=True)
 	update.message.reply_text('Video enviado')
 
+def pause(update, context):
+	cast = CattDevice(name=CAST_DEVICE)
+	cast.pause()
+	update.message.reply_text('Comando Pause enviado')
+
+def status():
+	sendMessage("Sigo Vivo")
+	Timer(60,status).start()
+
+def sendMessage(message):
+	bot_token = BOT_TEL_KEY
+	bot_chatID = CHAT_ID
+	bot_message = message
+	send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+	response = requests.get(send_text)
+	print(response.json())
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+#status()
+
+#def job():
+#	p = subprocess.Popen("catt scan | grep -v \"Scanning Chromecasts...\"", stdout=subprocess.PIPE, shell=True)
+#	(output, err) = p.communicate()
+#	print ("Command output : ", output)
+#	sendMessage(str(output, 'utf-8'))
+
+#schedule.every(1).minutes.do(job)
 
 def main():
     updater = Updater(BOT_TEL_KEY, use_context=True)
@@ -49,6 +81,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("cast", cast))
+    dp.add_handler(CommandHandler("pause", pause))
    
     dp.add_error_handler(error)
 
@@ -56,6 +89,9 @@ def main():
 
     updater.idle()
 
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
+
 if __name__ == '__main__':
     main()
-
