d4fbf8f07fe755d8096c8abb023842c0150af848
rafavg77 <rafavg77@gmail.com>
2020-05-17 02:42:21 -0500

    Commit Inicial, el bot ya funciona

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import logging
+from catt.api import CattDevice
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 BOT_TEL_KEY=os.environ['BOT_TEL_KEY']
@@ -11,6 +13,25 @@ BOT_NGR_KEY=os.environ['BOT_NGR_KEY']
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+def start(update, context):
+    username = update.message.chat.username
+    update.message.reply_text('Hi master %s,  I\'m here to serve you!' % (username))
+    logger.info("Command /start from %s" % (username))
+
+def help(update, context):
+    username = update.message.chat.username
+    update.message.reply_text('Este el comando de ayuda')
+    logger.info("Command /help from %s" % (username))
+
+def cast(update, context):
+	update.message.reply_text('Enviando video')
+	cast = CattDevice(name="TV Recamara")
+	VIDEOS = [ "https://www.youtube.com/watch?v=nUwEVoLB-tA" ]
+	for video in VIDEOS:
+		 cast.play_url(video, resolve=True, block=True)
+	update.message.reply_text('Video enviado')
+
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -24,6 +45,7 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("cast", cast))
    
     dp.add_error_handler(error)
 
